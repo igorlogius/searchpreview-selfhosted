4a3e8851e882f2b1ad4604989885c6eec5e90195
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-10-18 11:57:56 +0200

    feat: add sqlite Database

--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,68 @@
 const { Cluster } = require('puppeteer-cluster');
 const express = require('express');
+const fs = require('fs');
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
+
+const depricationTime = 60*60*24; // 1 day invalidate
+
+
+let db = (() => {
+
+    const thumbDBPath = 'previews.sqlite3';
+
+    if(fs.existsSync(thumbDBPath)){
+        return new sqlite3.Database(thumbDBPath);
+    }
+
+    let db = new sqlite3.Database(thumbDBPath);
+
+    db.serialize(() => {
+        db.run("CREATE TABLE thumbs (url TEXT PRIMARY KEY, thumb BLOB, updated INTEGER)");
+    });
+
+    return db;
+
+
+})();
+
+const insert_stmt = db.prepare("INSERT INTO thumbs (url, thumb, updated) VALUES(?,?,unixepoch()) ON CONFLICT(url) DO UPDATE SET thumb = ?, updated=unixepoch()");
+const select_stmt = db.prepare("SELECT thumb,(unixepoch()-updated) as blub FROM thumbs WHERE url = (?) AND blub < " + depricationTime);
+
+
+// return promise(row) on success
+function getThumb(url){
+    return new Promise( (resolve,reject) => {
+        select_stmt.get(url, (err,row) => {
+            if(err){
+                reject(err);
+            }else{
+                if(typeof row !== 'undefined' && typeof row.thumb !== 'undefined'){
+                    console.log('>> getThumb', row.blub);
+                    resolve(row.thumb);
+                }else{
+                    resolve(null);
+                }
+            }
+        });
+    });
+}
+
+// return Promise(null) on success
+function setThumb(url,thumb){
+    console.log('setThumb', url);
+    return new Promise( (resolve,reject) => {
+        insert_stmt.run(url,thumb,thumb, (err) => {
+            if(err !== null){
+                reject(err);
+            }else{
+                resolve(err);
+            }
+        });
+    });
+}
 
 // base64 => imgdata
-const imgCache = new Map();
 let cluster;
 
 (async () => {
@@ -12,6 +71,7 @@ let cluster;
     maxConcurrency: 4,
   });
   await cluster.task(async ({ page, data: url }) => {
+    await page.setJavaScriptEnabled(false); // disable JS execution
     await page.goto(url);
     return page.screenshot({'type': 'webp', 'quality': 10});
   });
@@ -20,14 +80,15 @@ let cluster;
 app.get('/api', async (req, res) => {
     if(req.query && req.query.url){
         const urlobj = new URL(decodeURIComponent(req.query.url));
-        const url = urlobj.origin;
-        if( !imgCache.has(url) ) {
-            imgCache.set(url, await cluster.execute(url));
+        const url = urlobj.toString(); //urlobj.origin;
+        let thumb = await getThumb(url);
+        if( thumb === null) {
+            console.log('creating preview image for',url);
+            thumb = await cluster.execute(url);
+            setThumb(url,thumb);
         }
-        if( imgCache.has(url) ) {
-            res.writeHead(200, {'Content-Type': 'image/webp'});
-            return res.end(imgCache.get(url));
-        }
+        res.writeHead(200, {'Content-Type': 'image/webp'});
+        return res.end(thumb);
     }
     return res.status(404).send("Not Found");
 })
